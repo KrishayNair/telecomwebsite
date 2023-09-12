730fcda10ad828766dcab0077834331ae335839c
Shantanu Gondane <113772055+shantanugondane@users.noreply.github.com>
2023-09-12 17:15:52 +0530

    basic

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -8,14 +8,15 @@ const Footer = () => {
         <div className="flexColStart f-left">
           <img src="./logo3.png" alt="" width={120} />
           <span className="secondaryText">
-            Our vision is to make all people <br />
-            the best place to live for them.
+            Our vision is to give  <br />
+            best services to our clients.
           </span>
         </div>
 
         <div className="flexColStart f-right">
           <span className="primaryText">Information</span>
-          <span className="secondaryText">145 New York, FL 5467, USA</span>
+          <span className="secondaryText">A32/A, The Old Shanti Niketan CHS,7 Bungalow Andheri West, Mumbai ,Capatain Harminder Singh Rd, Mumbai-400061, Maharastra
+</span>
           <div className="flexCenter f-menu">
             <span>Property</span>
             <span>Services</span>
--- src/components/GetStarted/GetStarted.jsx
+++ src/components/GetStarted/GetStarted.jsx
@@ -5,11 +5,11 @@ const GetStarted = () => {
     <div id="get-started" className="g-wrapper">
       <div className="paddings innerWidth g-container">
         <div className="flexColCenter inner-container">
-          <span className="primaryText">Get started with Homyz</span>
+          <span className="primaryText">Get started with Communicore Infra</span>
           <span className="secondaryText">
             Subscribe and find super attractive price quotes from us.
             <br />
-            Find your residence soon
+            Find your internet infra soon
           </span>
           <button className="button" href>
             <a href="mailto:zainkeepscode@gmail.com">Get Started</a>
